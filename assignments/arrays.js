48e42022da41653dcf30f4cf7e94a4a697f165e8
Divya Nair <divnairs@gmail.com>
2019-02-25 12:49:49 -0800

    arrays: 5

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -104,15 +104,19 @@ console.log(carModels)
 /* ==== Challenge 4 ====
 The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 */
-let carYears = inventory.map(car => car.car_year)
+const carYears = inventory.map(car => car.car_year)
 console.log(carYears)
 
-// ==== Challenge 5 ====
-// The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
-let oldCars = []
-console.log()
+/* 
+==== Challenge 5 ====
+The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging its length.
+*/
+const oldCars = carYears.filter(year => year < 2000)
+console.log(`The lot has ${oldCars.length} cars that are older than the year 2000.`)
 
-// ==== Challenge 6 ====
-// A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi = []
+/* 
+==== Challenge 6 ====
+A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
+*/
+const BMWAndAudi = []
 console.log()
