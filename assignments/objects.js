198e8d8318b48fa0f59b9f40c955f7fb28739966
Divya Nair <divnairs@gmail.com>
2019-02-25 14:26:38 -0800

    objects: stretch

--- assignments/objects.js
+++ assignments/objects.js
@@ -112,16 +112,36 @@ const parent = {
   child: {
     name: 'George',
     age: 50,
+    speak: function() {
+      return `Hello, my name is ${this.name}!`
+    },
     grandchild: {
       name: 'Sam',
-      age: 30
+      age: 30,
+      speak: function() {
+        return `Hello, my name is ${this.name}!`
+      }
     }
+  },
+  speak: function() {
+    return `Hello, my name is ${this.name}!`
   }
 }
 
 // Log the parent object's name
+console.log(parent.name)
+
 // Log the child's age
+console.log(parent.child.age)
+
 // Log the name and age of the grandchild
+console.log(parent.child.grandchild.name, parent.child.grandchild.age)
+
 // Have the parent speak
+console.log(parent.speak())
+
 // Have the child speak
+console.log(parent.child.speak())
+
 // Have the grandchild speak
+console.log(parent.child.grandchild.speak())
