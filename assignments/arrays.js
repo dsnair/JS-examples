040eceee30b4f2c6b044f4388c05a39921cbe02f
Divya Nair <divnairs@gmail.com>
2019-02-25 12:40:00 -0800

    arrays: 3

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -98,8 +98,8 @@ console.log(
 ==== Challenge 3 ====
 The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console.
 */
-const carModels = []
-console.log()
+const carModels = inventory.map(car => car.car_model).sort()
+console.log(carModels)
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
